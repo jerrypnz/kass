16108a1e26b39ae9a1691b49fb65dbb2008bf510
Jerry Peng <pr2jerry@gmail.com>
2019-07-22 22:28:46 +1200

    JSON serialization first cut!

---
+++ src/json.rs
@@ -0,0 +1,92 @@
+extern crate serde_json;
+
+use std::convert::Into;
+
+use serde_json::map::Map;
+use serde_json::Value;
+
+use cdrs::error::Result;
+use cdrs::frame::frame_result::{ColType, RowsMetadata};
+use cdrs::types::rows::Row;
+use cdrs::types::IntoRustByIndex;
+
+// TODO Replace `Into` with a different trait so that we can implement
+// it for other CDRS types like Udt, List, Map etc.
+fn column_to_json<R, T>(i: usize, row: &T) -> Result<Value>
+where
+    R: Into<Value>,
+    T: IntoRustByIndex<R>,
+{
+    let value = row.get_by_index(i)?;
+    match value {
+        Some(value) => Ok(value.into()),
+        None => Ok(Value::Null),
+    }
+}
+
+pub fn row_to_json(meta: &RowsMetadata, row: &Row) -> Result<Value> {
+    let mut i = 0;
+    let mut obj = Map::with_capacity(meta.columns_count as usize);
+
+    for col in &meta.col_specs {
+        let name = col.name.as_plain();
+        let value = match &col.col_type.id {
+            // strings
+            ColType::Varchar | ColType::Ascii => column_to_json::<String, _>(i, row)?,
+            // integers
+            ColType::Tinyint => column_to_json::<i8, _>(i, row)?,
+            ColType::Smallint => column_to_json::<i16, _>(i, row)?,
+            ColType::Int => column_to_json::<i32, _>(i, row)?,
+            ColType::Bigint | ColType::Counter => column_to_json::<i64, _>(i, row)?,
+            // floats
+            ColType::Float => column_to_json::<f32, _>(i, row)?,
+            ColType::Double => column_to_json::<f64, _>(i, row)?,
+            // bool
+            ColType::Boolean => column_to_json::<bool, _>(i, row)?,
+            // TODO format date time according to CLI option
+            ColType::Time | ColType::Timestamp => column_to_json::<i64, _>(i, row)?,
+            // null
+            ColType::Null => Value::Null,
+            //TODO Implement other types: Blob, Udt etc
+            _ => Value::Null,
+        };
+        obj.insert(name, value);
+        i = i + 1;
+    }
+    Ok(obj.into())
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    struct MockRow<R> {
+        val: R,
+    }
+
+    impl<R> MockRow<R> {
+        pub fn new(v: R) -> MockRow<R> {
+            MockRow { val: v }
+        }
+    }
+
+    impl<R: Clone> IntoRustByIndex<R> for MockRow<R> {
+        fn get_by_index(&self, _: usize) -> Result<Option<R>> {
+            Ok(Some(self.val.clone()))
+        }
+
+        fn get_r_by_index(&self, _: usize) -> Result<R> {
+            Ok(self.val.clone())
+        }
+    }
+
+    #[test]
+    pub fn string_to_json() {
+        let s = "hello world";
+        let mock_row = MockRow::new(String::from(s));
+        let json_res =
+            column_to_json::<String, _>(0, &mock_row).expect("failed to convert to json");
+        assert_eq!(Value::from(s), json_res);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,17 @@
 extern crate cdrs;
 extern crate clap;
 
-use std::fmt::Display;
+mod json;
 
 use clap::{App, Arg};
 
 use cdrs::authenticators::NoneAuthenticator;
 use cdrs::cluster::session::{new as new_session, Session};
 use cdrs::cluster::{ClusterTcpConfig, NodeTcpConfigBuilder, TcpConnectionPool};
+use cdrs::error::Result;
 use cdrs::load_balancing::RoundRobin;
 use cdrs::query::*;
-use cdrs::types::rows::{Row};
-use cdrs::types::{IntoRustByIndex};
-use cdrs::error::Result;
-use cdrs::frame::frame_result::{RowsMetadata, ColType};
+use serde_json;
 
 type CurrentSession = Session<RoundRobin<TcpConnectionPool<NoneAuthenticator>>>;
 
@@ -25,32 +23,8 @@ fn connect(host: &str) -> CurrentSession {
     session
 }
 
-fn print_val<R: Display, T: IntoRustByIndex<R>>(i: usize, row: &T) {
-    let value = row.get_by_index(i).expect("Failed to get value");
-    match value {
-        Some(value) => print!("{}\t", value),
-        None => print!("\t"),
-    };
-}
-
-fn print_row(meta: &RowsMetadata, row: &Row) {
-    let mut i = 0;
-    for col in &meta.col_specs {
-        match &col.col_type.id {
-            ColType::Int => print_val::<i32, Row>(i, row),
-            ColType::Varchar => print_val::<String, Row>(i, row),
-            ColType::Null => print!("null\t"),
-            _ => print!("\t"),
-        };
-        i = i+1;
-    }
-    println!("");
-}
-
 fn query(session: &CurrentSession, cql: &str) -> Result<()> {
-    let body = session
-        .query(cql)
-        .and_then(|x| x.get_body())?;
+    let body = session.query(cql).and_then(|x| x.get_body())?;
 
     let meta = body.as_rows_metadata();
     let rows = body.into_rows();
@@ -58,9 +32,15 @@ fn query(session: &CurrentSession, cql: &str) -> Result<()> {
     match rows {
         Some(rows) => {
             let meta = meta.unwrap();
-            println!("Query returned {} rows, {} columns", rows.len(), meta.columns_count);
+            // println!(
+            //     "Query returned {} rows, {} columns",
+            //     rows.len(),
+            //     meta.columns_count
+            // ) ;
             for row in rows {
-                print_row(&meta, &row);
+                let json = json::row_to_json(&meta, &row)?;
+                let json_str = serde_json::to_string(&json).expect("failed to print json");
+                println!("{}", json_str);
             }
         }
         None => println!("Query didn't return a result"),
@@ -74,16 +54,20 @@ fn main() -> Result<()> {
         .version("0.1.0")
         .author("Jerry Peng <pr2jerry@gmail.com>")
         .about("Command line Cassandra CQL client")
-        .arg(Arg::with_name("host")
-             .short("h")
-             .long("host")
-             .value_name("HOST:PORT")
-             .help("The Cassandra host to connect to")
-             .takes_value(true))
-        .arg(Arg::with_name("QUERY")
-             .help("The query to run")
-             .required(true)
-             .index(1))
+        .arg(
+            Arg::with_name("host")
+                .short("h")
+                .long("host")
+                .value_name("HOST:PORT")
+                .help("The Cassandra host to connect to")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("QUERY")
+                .help("The query to run")
+                .required(true)
+                .index(1),
+        )
         .get_matches();
 
     let host = matches.value_of("host").unwrap_or("127.0.0.1:19142");
